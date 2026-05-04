d38ffebbd820eea2d56afd10cf81b5a334d9f112
MernaSobhi <mernasobhi3@gmail.com>
2026-05-04 03:44:49 +0300

    solve conflict

--- Wasaly.DAL/Data/Context/ApplicationDbContext.cs
+++ Wasaly.DAL/Data/Context/ApplicationDbContext.cs
@@ -8,7 +8,6 @@ using Wasaly.DAL.Models;
 namespace Wasaly.DAL.Data.Context
 {
     public class ApplicationDbContext : IdentityDbContext<WasalyIdentityUser, IdentityRole, string>
-    public class ApplicationDbContext : IdentityDbContext<WasalyIdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
--- Wasaly.PL/Program.cs
+++ Wasaly.PL/Program.cs
@@ -13,18 +13,15 @@ namespace Wasaly.PL
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString, sqlOptions =>
-                {
-                    sqlOptions.EnableRetryOnFailure();
-                }));
+                options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 
+
             builder.Services.AddIdentity<WasalyIdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
-                .AddEntityFrameworkStores <ApplicationDbContext>();
+                .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllersWithViews();
 
             builder.Services.AddRazorPages();
